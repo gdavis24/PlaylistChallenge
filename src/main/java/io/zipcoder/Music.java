9860b0bbb0822655b8996e80115c556dfb7c2829
Gregory Davis <gdavis@zipcodes-MacBook-Pro.local>
2020-02-16 14:35:24 -0500

    finished lab

--- src/main/java/io/zipcoder/Music.java
+++ src/main/java/io/zipcoder/Music.java
@@ -9,6 +9,30 @@ public class Music {
     }
 
     public Integer selection(Integer startIndex, String selection){
-        return null;
+        int goUp = 0;
+        int goDown = 0;
+
+        for (int i = startIndex; i < playList.length; i++) {
+            if (!(playList[i % playList.length].equals(selection))){
+                goUp++;
+            }
+            else{
+                break;
+            }
+        }
+
+        for (int i = startIndex; i >= 0; i--) {
+            if (!(playList[i % playList.length].equals(selection))){
+                goDown++;
+            }
+            else{
+                break;
+            }
+        }
+        if (goUp > goDown){
+            return goDown;
+        }
+
+        return goUp;
     }
 }
